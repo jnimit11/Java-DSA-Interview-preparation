27eeb3560a6131eb81d6642a1363f863f4cde4b4
Nimit Jain <jainnimit11@gmail.com>
2026-06-23 09:34:55 +0530

    strings in java

---
+++ strings/strings.java
@@ -0,0 +1,17 @@
+public class strings {
+    public static void main(String[] args) {
+        String name = "Nimit";
+        System.out.println(name);
+
+        // ways of creating a string
+        // 1. string literal(static memory)
+        String str = "This is string literal";
+        System.out.println(str);
+        // 2. Using new keyword (heap memory)
+        // Using the new keyword creates a new object in heap memory, even if the same
+        // string already exists in the pool.
+
+        
+
+    }
+}
